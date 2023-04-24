4f9230c51017f5835bfccf3f8d7631de2096b87d
Hai Anh <67957887+haianhxanh@users.noreply.github.com>
2023-04-24 18:41:55 +0200

    Init

---
+++ learn/vite-app/src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+import ListGroup from "./components/ListGroup";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <ListGroup />
+    </div>
+  );
+}
+
+export default App;
